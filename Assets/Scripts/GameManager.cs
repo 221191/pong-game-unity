6f1260ddfee6f17b9ac1180a6e6b0110499bb246
Vasa <nemanjavasilev@gmail.com>
2023-05-23 16:21:09 +0200

    add game manager script

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.UI;
+public class GameManager : MonoBehaviour
+{
+    private int _playerScore;
+    private int _computerScore;
+
+    public TMPro.TMP_Text playerScoreText;
+    public TMPro.TMP_Text computerScoreText;
+
+    public Ball ball;
+    public Paddle playerPaddle;
+    public Paddle computerPaddle;
+
+    public void PlayerScores(){
+        _playerScore++;
+        // Debug.Log(_playerScore);
+        this.playerScoreText.text = _playerScore.ToString();
+        ResetRound();
+    }
+
+    public void ComputerScore(){
+        _computerScore++;
+        //  Debug.Log(_computerScore);
+        this.computerScoreText.text = _computerScore.ToString();
+        ResetRound();
+    }
+
+    private void ResetRound(){
+        this.playerPaddle.ResetPosition();
+        this.computerPaddle.ResetPosition();
+        this.ball.ResetPosition();
+        this.ball.AddStartingForce();
+    }
+}
